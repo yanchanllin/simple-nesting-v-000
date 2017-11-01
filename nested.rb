7872187d2b1b1ccde0eeed30dd0652aac002a554
YanChan  Lin <yonalin1@gmail.com>
2017-11-01 03:51:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+:alan_kay = {:known_for => "Object Orientation"}
 	programmer_hash = 
  		{
         :grace_hopper => {
